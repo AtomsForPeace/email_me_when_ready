fb6240ae6f29684879a1967c419171bf62c0ae35
AtomsForPeace <adam.p.bannister@gmail.com>
2016-02-14 14:50:07 +0100

    add encoding etc.

--- email_me_when_ready/__init__.py
+++ email_me_when_ready/__init__.py
@@ -1,1 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from .email_me_when_ready import EmailMeWhenReady
--- email_me_when_ready/email_me_when_ready.py
+++ email_me_when_ready/email_me_when_ready.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import smtplib, configparser
 from datetime import datetime
 
